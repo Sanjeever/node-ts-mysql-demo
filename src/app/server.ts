94b2d9e3cb7714b20032ba0b2fcf51d2fca0c758
Sanjeever <78210374+Sanjeever@users.noreply.github.com>
2023-06-26 19:06:54 +0800

    ✨feat: add consola for logging and success message

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,10 +1,11 @@
+import { consola } from 'consola'
 import { execSQL } from '@/db/mysql'
 
 const sql: string = `select host,user from user;`
 
 const main = async () => {
   const result = await execSQL(sql)
-  console.log(result)
+  consola.success(result, '\n')
   process.exit()
 }
 
