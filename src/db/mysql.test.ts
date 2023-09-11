507b12175f26d6cd7e48d2e3e26aa5f801ff4e90
Sanjeever <78210374+Sanjeever@users.noreply.github.com>
2023-09-11 19:51:08 +0800

    🦄 refactor: use log4js

--- src/db/mysql.test.ts
+++ src/db/mysql.test.ts
@@ -1,8 +1,12 @@
 import { test } from 'vitest'
 import { execSQL } from './mysql'
+import log4js from 'log4js'
+
+const logger = log4js.getLogger()
+logger.level = 'debug'
 
 test('execSQL()', async () => {
   const sql: string = `select host,user from user;`
   const result = await execSQL(sql)
-  console.log(result)
+  logger.info(result)
 })
